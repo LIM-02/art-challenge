3ec21c4bbecf9d4e91ecf60980669d28d16db41a
LIM-02 <limzhicheng2216@gmail.com>
2024-12-03 18:43:11 +0800

    update database connection

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 DATABASE = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'Lzc_011759',
-    'database': 'warehouse'
+    'host': 'fojvtycq53b2f2kx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    'user': 'mupqluvzz6s6b6hy',
+    'password': 'jb6qb512yihaao2v',
+    'database': 'tncumtqghbmwlxr6'
 }
