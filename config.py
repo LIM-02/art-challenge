b72f6928a1ca0ea78159fa92de909856a90f029d
LIM-02 <limzhicheng2216@gmail.com>
2024-12-08 14:15:39 +0800

    update the database

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DATABASE = {
-    'host': 'localhost',
+    'host': '34.87.71.21',  # Cloud SQL Public IP
     'user': 'root',
     'password': 'Lzc_011759',
-    'database': 'warehouse'
+    'database': 'warehouse',
+    'port': 3306  # Optional if you're using the default port
 }
